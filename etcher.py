ba466eaaa6c02d63e941387152b1588735ea42b8
Padlex <alexpadula2001@gmail.com>
2019-09-28 12:58:32 +0200

    better resolution

--- etcher.py
+++ etcher.py
@@ -9,18 +9,20 @@ img_path = "2channel_crop.png"
 output_name = "2channel_crop"
 
 # (x, y)
-offset = (5, 5)
-engrave_width = 10
+offset = (10, 10)
+engrave_width = 100
 
 # mm/min
-engrave_speed = 300
+engrave_speed = 700
 move_speed = 3000
 
-# Higher resolutions will result in longer gcode and possibly longer time
-image_resolution = ()
-
 # row/mm
-row_density = 5
+row_density = 7
+
+# Between 0 (not inclusive) and 1 (inclusive)
+# Higher resolutions will result in longer gcode and longer engraving time
+# image_resolution = 0.1
+
 
 # Commands
 
@@ -47,50 +49,58 @@ def image_to_gcode():
     # Open Image
     im = Image.open(img_path)
 
+    # Controls the physical size of the engraved image
     resize_ratio = im.width/engrave_width
     new_size = (int(im.width/resize_ratio), int(im.height/resize_ratio))
 
-    #im = im.resize(new_size, resample=Image.LANCZOS)
+    # Controls the level of detail
+    resolution_ratio = im.height/(new_size[1]*row_density)
+    im = im.resize((int(im.width / resolution_ratio), int(im.height / resolution_ratio)), resample=Image.LANCZOS)
+
+    im.show()
 
     # Rotate to move horizontally rather than vertically
-    # im = im.rotate(90, expand=True)
+    im = im.transpose(Image.FLIP_TOP_BOTTOM)
 
     pixels = np.array(im)
 
     print(pixels.shape)
 
-    row_dist = 1/row_density
+    # row_dist = 1/row_density
 
     # Generate g-code
     for y, row in enumerate(pixels):
 
-        for r in range(row_density):
-            # Move to next row
-            # ToDo snake
-            gcode += laser_off + '\n'
-            previous_pow = -1
+        # Skip row if empty
+        if np.mean(row) == 0:
+            continue
 
-            # next row is at height offset + y pixel
-            gcode += quick_move + "X%d Y%f \n" % (offset[0], offset[1] + y + row_dist * r)
+        # Move to new row
+        # ToDo snake
+        gcode += laser_off + '\n'
+        previous_pow = -1
 
-            for x, pixel in enumerate(row):
-                pow = np.mean(pixel)
+        # next row is at height offset + y pixel
+        gcode += quick_move + "X%f Y%f \n" % (offset[0], offset[1] + y/im.height * new_size[1])
 
-                # Init row
-                if x == 0:
-                    previous_pow = pow
-                    continue
+        for x, pixel in enumerate(row):
+            pow = abs(np.mean(pixel)-255)
 
-                # Wait for a change in power or end of row to issue new commands
-                if pow != previous_pow or x == len(row) - 1:
+            # Init row
+            if x == 0:
+                previous_pow = pow
+                continue
 
-                    # Skip if blank, ie pow == 0
-                    if pow > 0:
-                        # Move to previous position with previous power
-                        gcode += set_power + "S%d \n" % previous_pow
-                        gcode += move + "X%d \n" % (offset[0] + x-1)
+            # Wait for a change in power or end of row to issue new commands
+            if pow != previous_pow or x == len(row) - 1:
 
-                    previous_pow = pow
+                # Skip if blank, ie pow == 0
+                if pow > 0:
+                    # Move to previous position with previous power
+                    gcode += set_power + "S%d \n" % previous_pow
+                    gcode += move + "X%f \n" % (offset[0] + (x-1)/im.width * new_size[0])
+
+                previous_pow = pow
 
 
 
--- opening.gcode
+++ opening.gcode
@@ -1,1 +1,4 @@
+G28 ; Go to origin on all axes
+G92 X0 Y0 Z0.8
 G90 ; Set all axes to absolute
+G0 Z0.8 : move to correct height based on focus and material thickness
